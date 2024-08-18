ef061fca991e03896309c5c1d5422397791b55d1
Ambith A T <33983642+ambith@users.noreply.github.com>
2024-08-18 21:11:23 +0530

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -12,7 +12,7 @@ npm install
 chmod -R 775 /app
 chown -R expense:expense /app
 dnf install mysql-server -y
-mysql -h 3.91.103.247 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.43.167 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 systemctl daemon-reload
 systemctl enable backend 
 systemctl start backend 
